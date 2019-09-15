ad4216868f0c55e6b6e6e8f58fde61fc9a76177f
Bruno1903 <brunoalmeida1960@gmail.com>
2019-09-15 15:43:13 -0300

    Tipos em Java

---
+++ nivelamento-java-1/src/main/java/com/mycompany/nivelamento/java/TiposJava.java
@@ -0,0 +1,34 @@
+package com.mycompany.nivelamento.java;
+
+public class TiposJava {
+
+    // método main()
+    // (psvm e depois tecla <TAB>)
+    public static void main(String[] args) {
+        
+        // sout e depois tecla <TAB>
+        System.out.println("É nóis no Java");
+        
+        // tipos Java
+        // Java é uma linguagem de Tipagem Estática
+        
+        // Detalhe: A convenção de nomes de variáveis
+        // é o padrão camelCase
+        
+        // Tipo Texto: String
+        String cidade = "São Paulo";
+        
+        // Tipo Númerico Inteiro: Integer
+        Integer populacao = 15000000; // pode ser 15_000_000
+        
+        // Tipo Numérico Real: Double
+        Double temperaturaMedia = 23.1234;
+        
+        // Tipo Boleano (ou Lógico): Boolean
+        Boolean vivo = true;
+        Boolean temMundial = false;
+        
+        
+    }
+    
+}
